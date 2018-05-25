cc42df4e28782eb20c2e42f718eb0641d72c0c4e
Demian Florentin <demian@sinergetica.com>
2018-05-25 17:38:32 -0400

    basic hoc and posts getter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import request from 'request-promise';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const url = 'https://thewirecutter.com/wp-json/wp/v2/posts';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getPosts = this.getPosts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    request(url).then((rawData) => {
+      const data = JSON.parse(rawData);
+      console.log(data);
+      this.setState({ posts: data.posts });
+    }).catch((err) => { throw err; });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div />);
   }
 }
 
